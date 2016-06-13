dd740208b7fda9ec661a8e1cb7e2f560d1e810ef
jackgerrits <jackgerrits95@gmail.com>
2016-06-13 16:36:52 +0930

    initial commit of jekyll site

---
+++ archive/2015/script.js
@@ -0,0 +1,39 @@
+var content = {
+	ABOUT: 'ABOUT',
+	CONTACT: 'CONTACT',
+	NONE: 'NONE'
+}
+
+$(document).ready(function(){
+	current = 'NONE';
+
+	$('#about').click( function() {
+		if(current == content.NONE) {
+			$('#aboutContainer').fadeIn();
+			current = content.ABOUT;
+		} else if (current == content.CONTACT) {
+			$('#contactContainer').fadeOut({ complete: function() {
+				$('#aboutContainer').fadeIn();
+				current = content.ABOUT;
+			}});
+		} else if (current == content.ABOUT){
+			$('#aboutContainer').fadeOut();
+			current = content.NONE;
+		}	
+	});
+
+	$('#contact').click( function() {
+		if(current == content.NONE) {
+			$('#contactContainer').fadeIn();
+			current = content.CONTACT;
+		} else if (current == content.ABOUT) {
+			$('#aboutContainer').fadeOut({complete: function() {
+				$('#contactContainer').fadeIn();
+				current = content.CONTACT;
+			}});
+		} else if (current == content.CONTACT){
+			$('#contactContainer').fadeOut();
+			current = content.NONE;
+		}
+	});
+});
---
+++ js/main.js
@@ -0,0 +1,22 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+$(document).ready(function () {
+	  $(".navbar-toggle").on("click", function () {
+		    $(this).toggleClass("active");
+	  });
+});
